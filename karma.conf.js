fe529eb0d029547cdf1c0de708b7249fae8f44de
Joe Johnson <joe.johnson3909@gmail.com>
2022-03-31 14:34:57 +1100

    Update node and project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Deadpool-ComponentLibrary'),
+      dir: require('path').join(__dirname, './coverage/gear-ratios'),
       subdir: '.',
       reporters: [
         { type: 'html' },
